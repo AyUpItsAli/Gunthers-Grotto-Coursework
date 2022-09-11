b5cc25420433c8978924e1c462aa5c08ff2b1e09
AliDee <ali.dee1410@gmail.com>
2022-09-11 11:42:24 +0100

    Enemy pathfinding
    - Every frame, the enemy obtains a path towards the player from the World node in the level scene.
    - A line is drawn to represent this path on screen, for debugging.

--- Actors/Enemies/RockHermit.gd
+++ Actors/Enemies/RockHermit.gd
@@ -2,9 +2,12 @@ extends KinematicBody2D
 
 # Node references
 onready var search_radius: Area2D = $SearchRadius
+onready var world: Navigation2D = get_parent().get_parent()
+onready var navigation_line: Line2D = $NavigationLine
 
 # Variables
 var player: KinematicBody2D # Reference to the player node, once detected
+var path: Array # Array of points that the enemy must follow
 
 func _ready():
 	search_radius.connect("body_entered", self, "body_entered_search_radius")
@@ -14,3 +17,13 @@ func body_entered_search_radius(body):
 		if body.name == "Player":
 			player = body
 
+# Stores an array of points leading towards the player in the path variable
+func determine_path_to_player():
+	if player:
+		path = world.get_simple_path(position, player.position, false)
+		navigation_line.points = path
+
+func _process(delta):
+	navigation_line.global_position = Vector2.ZERO
+	if player:
+		determine_path_to_player()
