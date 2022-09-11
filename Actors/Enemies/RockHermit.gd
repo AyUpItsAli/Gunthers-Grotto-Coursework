337f34d26d2c160f2f9ac91766018012e4673576
Alastair <ali.dee1410@gmail.com>
2022-09-11 17:06:06 +0100

    Enemy movement
    - Gave enemy velocity vector.
    - Enemy's velocity is directed towards the next point along their path.
    - Disabled regular collisions for the enemy, to stop it getting stuck on walls.
    - Added "soft collisions" system, so that enemies "avoid" each other and other objects that also have soft collisions.
    - Added soft collisions to the player and stalagmites, so enemies go "around" them.

--- Actors/Enemies/RockHermit.gd
+++ Actors/Enemies/RockHermit.gd
@@ -1,13 +1,19 @@
 extends KinematicBody2D
 
+# Constants
+const MOVE_SPEED = 2500
+const STOP_DISTANCE = 30
+
 # Node references
 onready var search_radius: Area2D = $SearchRadius
 onready var world: Navigation2D = get_parent().get_parent()
 onready var navigation_line: Line2D = $NavigationLine
+onready var soft_collision: Area2D = $SoftCollision
 
 # Variables
 var player: KinematicBody2D # Reference to the player node, once detected
 var path: Array # Array of points that the enemy must follow
+var velocity = Vector2.ZERO
 
 func _ready():
 	search_radius.connect("body_entered", self, "body_entered_search_radius")
@@ -27,3 +33,22 @@ func _process(delta):
 	navigation_line.global_position = Vector2.ZERO
 	if player:
 		determine_path_to_player()
+
+# Determines the enemy's velocity to move along the path
+func determine_velocity(delta):
+	velocity = Vector2.ZERO
+	if player and path.size() > 0:
+		if position.distance_to(player.position) > STOP_DISTANCE:
+			# If reached first point
+			# Remove point from list
+			if position == path[0]:
+				path.pop_front()
+			# Set velocity towards the next point
+			velocity = position.direction_to(path[0]) * MOVE_SPEED * delta
+	
+	if soft_collision.is_colliding():
+		velocity += soft_collision.get_push_vector() * MOVE_SPEED * delta
+
+func _physics_process(delta):
+	determine_velocity(delta)
+	velocity = move_and_slide(velocity)
