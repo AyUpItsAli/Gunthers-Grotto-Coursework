ed1428416d86adf705f553d0030e12d37485cbf6
AliDee <ali.dee1410@gmail.com>
2022-09-16 23:50:48 +0100

    Player inventory
    - Added Item Ids to Globals
    - Added dictionary contain item information for each item id in Globals
    - Added inventory dictionary to PlayerData script
    - Added add and remove functions for player inventory

--- Globals/Globals.gd
+++ Globals/Globals.gd
@@ -3,3 +3,23 @@ extends Node
 # Cave Generation
 const CAVE_SIZE = 50 # number of tiles
 const CAVE_TILE_SIZE = 32 # number of pixels
+
+# Items
+enum ItemIDs { GEM, REVOLVER_AMMO, DYNAMITE_STICK }
+const ITEMS = {
+	ItemIDs.GEM: {
+		single = "Gem",
+		plural = "Gems",
+		texture = null
+	},
+	ItemIDs.REVOLVER_AMMO: {
+		single = "Bullet",
+		plural = "Bullets",
+		texture = null
+	},
+	ItemIDs.DYNAMITE_STICK: {
+		single = "Dynamite Stick",
+		plural = "Dynamite Sticks",
+		texture = null
+	}
+}
--- Globals/PlayerData.gd
+++ Globals/PlayerData.gd
@@ -1,9 +1,35 @@
 extends Node
 
 var health = 5
+var inventory = {} # key: Item ID | value: Quantity
 
 func reduce_health(amount: int):
 	health -= amount
 	if health <= 0:
 		health = 0
 		print("Game Over")
+
+# Attempts to ADD an item to the inventory
+# Returns whether the item was able to be added or not
+func add_item(item_id: int, amount: int = 1) -> bool:
+	if not item_id in Globals.ITEMS: return false
+	if item_id in inventory:
+		inventory[item_id] += amount
+	else:
+		inventory[item_id] = amount
+	return true
+
+# Attempts to REMOVE an item from the inventory
+# Returns whether the item was able to be removed or not
+func remove_item(item_id: int, amount: int = 1) -> bool:
+	if not item_id in Globals.ITEMS: return false
+	if not item_id in inventory: return false
+	var current_quantity: int = inventory[item_id]
+	if amount > current_quantity:
+		return false
+	elif amount == current_quantity:
+		inventory.erase(item_id)
+		return true
+	else:
+		inventory[item_id] -= amount
+		return true
