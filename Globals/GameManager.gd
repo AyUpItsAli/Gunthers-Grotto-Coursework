1f8bb423907e81a32474bf21dc83d99b6c1bdc22
Alastair <ali.dee1410@gmail.com>
2022-09-17 13:18:09 +0100

    Added comments to GameManager script

--- Globals/GameManager.gd
+++ Globals/GameManager.gd
@@ -15,8 +15,11 @@ func create_pool(frequencies: Dictionary) -> Array:
 			pool.append(item) # Add this item to the pool, n times
 	return pool
 
+# Takes in a float from 0 to 100 representing a percentage chance
+# Returns whether the chance was successful or not
 func percent_chance(chance: float) -> bool:
 	return rng.randf_range(0, 99) < chance
 
+# Picks a random item from the gem quantity pool
 func get_random_gem_quantity() -> int:
 	return gem_quantity_pool[randi() % gem_quantity_pool.size()]
