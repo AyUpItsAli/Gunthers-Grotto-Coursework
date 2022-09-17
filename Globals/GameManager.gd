656a750729fefb4ea990e768634fda1de246f060
Alastair <ali.dee1410@gmail.com>
2022-09-17 12:47:29 +0100

    Added gem quantity pool to GameManager
    - Makes more sense to keep variables outside of Globals
    - Added get_random_gem_quantity to GameManagert to abstract gem quantity pool

--- Globals/GameManager.gd
+++ Globals/GameManager.gd
@@ -3,18 +3,20 @@ extends Node
 # Random number generator used throughout the game
 onready var rng := RandomNumberGenerator.new()
 
-func percent_chance(chance: float) -> bool:
-	return rng.randf_range(0, 99) < chance
+# A pool of gem quantities generated from Globals.GEM_FREQUENCIES
+onready var gem_quantity_pool = create_pool(Globals.GEM_FREQUENCIES)
 
 # Creates a pool of items from the given dictionary of item frequencies
 func create_pool(frequencies: Dictionary) -> Array:
 	var pool = []
 	for item in frequencies: # For each item...
-		var n = frequencies[item] # Store the frequency of this item (n)
+		var n = frequencies[item] # Store the frequency of this item as "n"
 		for i in range(n):
 			pool.append(item) # Add this item to the pool, n times
 	return pool
 
-# Chooses an item from the given pool
-func choose_from(pool: Array):
-	return pool[randi() % pool.size()]
+func percent_chance(chance: float) -> bool:
+	return rng.randf_range(0, 99) < chance
+
+func get_random_gem_quantity() -> int:
+	return gem_quantity_pool[randi() % gem_quantity_pool.size()]
