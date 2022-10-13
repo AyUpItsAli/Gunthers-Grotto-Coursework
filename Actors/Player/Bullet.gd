cdbb844fbe267a1afaaacfbe348eae7119d119e3
Alastair <ali.dee1410@gmail.com>
2022-10-13 10:37:41 +0100

    Bullet improvements

--- Actors/Player/Bullet.gd
+++ Actors/Player/Bullet.gd
@@ -1,23 +1,26 @@
 extends Area2D
 
-const MOVE_SPEED = 10
+# Constants
+const BULLET_SPEED = 100
 
-onready var hurtbox: Area2D = $Hurtbox
-
+# Variables
 var velocity: Vector2
 
 func _ready():
 	connect("body_entered", self, "on_collision")
-	connect("area_entered", self, "on_collision")
-	hurtbox.connect("area_entered", self, "on_area_entered_hurtbox")
+	$Hurtbox.connect("area_entered", self, "on_area_entered_hurtbox")
 
 func _physics_process(delta):
-	position += velocity * MOVE_SPEED * delta
+	position += velocity * BULLET_SPEED * delta
 
-# Called when the bullet collides with anything (bodies and areas included)
-func on_collision(body_or_area):
+# Called when the bullet collides with something "physical"
+# The bullet should be destroyed on impact
+func on_collision(body):
 	queue_free()
 
 # Called when an area enters the bullet's hurtbox
 func on_area_entered_hurtbox(area):
-	print("hurt enemy")
+	print("hit enemy!")
+	var enemy = area.get_parent()
+	# As the enemy is not a "body", invoke on_collision() manually
+	on_collision(enemy)
