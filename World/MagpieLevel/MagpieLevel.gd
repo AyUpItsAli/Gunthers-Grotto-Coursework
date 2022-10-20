84482528d388ae59c67ac8c485b7729005a1338c
AliDee <ali.dee1410@gmail.com>
2022-10-20 18:04:22 +0100

    Cave Exit in Magpie Level returns player to main Level

--- World/MagpieLevel/MagpieLevel.gd
+++ World/MagpieLevel/MagpieLevel.gd
@@ -3,12 +3,18 @@ extends Node2D
 # Node references
 onready var rock_layer = $RockLayer
 onready var player = $Objects/Player
+onready var cave_exit = $Objects/CaveExit
 
 # HUD
 onready var minimap = $HUD/UI/Minimap
 onready var loading_screen = $HUD/LoadingScreen
 
 func _ready():
+	cave_exit.connect("player_entered", self, "on_player_exited_cave")
+	
+	# Reset the number of caves since the magpie level spawned
+	GameManager.caves_since_magpie = 0
+	
 	# Update the minimap to display the layout of the magpie level
 	minimap.update_minimap(rock_layer)
 	
@@ -28,3 +34,11 @@ func _ready():
 func _process(delta):
 	var player_tile_pos = rock_layer.world_to_map(player.position)
 	minimap.update_player_pos(player_tile_pos)
+
+# Called when the player enters the CaveExit detection radius
+func on_player_exited_cave():
+	if not loading_screen.visible:
+		var animations = loading_screen.get_node("LoadingScreenAnimations")
+		animations.play("Fade_In")
+		yield(animations, "animation_finished")
+		get_tree().change_scene("res://World/Level/Level.tscn")
