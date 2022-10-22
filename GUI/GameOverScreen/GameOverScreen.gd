f7ab401c5a1b566e38f46a35e30721c207c48495
Alastair <ali.dee1410@gmail.com>
2022-10-22 21:38:35 +0100

    Display final score in Game Over screen

--- GUI/GameOverScreen/GameOverScreen.gd
+++ GUI/GameOverScreen/GameOverScreen.gd
@@ -1,9 +1,11 @@
 extends Control
 
+onready var final_score_label = $FinalScore/Label
 onready var menu_button = $MenuButton
 
 func _ready():
 	menu_button.connect("pressed", self, "on_menu_button_pressed")
+	final_score_label.text = "Final Score: " + str(GameManager.cave_depth)
 
 func on_menu_button_pressed():
 	# Prevent the user from pressing the button again
