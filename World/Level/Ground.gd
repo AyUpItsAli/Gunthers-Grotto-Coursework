593468ba3d93f80435501423aba32c4ddf1a5481
Ali <ali.dee1410@gmail.com>
2022-10-30 12:49:47 +0000

    Removed navigation info for ground tiles
    - Ground layer now only has a single GROUND tile, rather than navable and non-navable tiles.
    - Navigation information has been removed from the ground layer, as enemies won't be needing it anymore.

--- World/Level/Ground.gd
+++ World/Level/Ground.gd
@@ -4,13 +4,9 @@ extends TileMap
 onready var walls = get_parent().get_node("Walls")
 
 # Tile references
-const NAVABLE = 0
-const NON_NAVABLE = 1
+const GROUND = 0
 
 func update_ground_layer():
 	for x in range(Globals.CAVE_SIZE):
 		for y in range(Globals.CAVE_SIZE):
-			if walls.get_cell(x, y) == -1: # If empty, place navable...
-				set_cell(x, y, NAVABLE)
-			else: # Otherwise, place non-navable
-				set_cell(x, y, NON_NAVABLE)
+			set_cell(x, y, GROUND)
