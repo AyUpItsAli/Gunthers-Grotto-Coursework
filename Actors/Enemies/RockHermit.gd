eeebc8979867a74ef825da7bb2e30fed872d44bc
Ali <ali.dee1410@gmail.com>
2022-11-05 14:26:09 +0000

    RockHermit attacks during Chase state
    RockHermits now attack the player again.
    - AttackDelayTimer (0.5 secs) controls the delay between the player entering the enemy's hurtbox, and the enemy's first attack.
    - AttackCooldownTimer (1 secs) controls the delay between consecutive attacks, after the initial attack.

--- Actors/Enemies/RockHermit.gd
+++ Actors/Enemies/RockHermit.gd
@@ -6,11 +6,12 @@ const ATTACK_DAMAGE = 1
 
 # Node references
 onready var body_sprite: Sprite = $BodySprite
-onready var hurtbox: Area2D = $Hurtbox
-onready var attack_timer: Timer = $AttackTimer
-onready var detection_radius: Area2D = $DetectionRadius
-onready var target_ray: RayCast2D = $TargetRay
 onready var soft_collision: Area2D = $SoftCollision
+onready var detection_radius: Area2D = $DetectionRadius
+onready var hurtbox: Area2D = $Hurtbox
+onready var attack_delay_timer: Timer = $AttackDelayTimer # Delay before first attack
+onready var attack_cooldown_timer: Timer = $AttackCooldownTimer # Cooldown between consecutive attacks
+onready var target_ray: RayCast2D = $TargetRay
 onready var debug_ray: RayCast2D = $DebugRay # Used to display vectors for debugging
 onready var state_machine: StateMachine = $StateMachine
 
@@ -20,9 +21,22 @@ var last_known_location: Vector2
 var velocity = Vector2.ZERO
 var health = 5
 
+func _ready():
+	hurtbox.connect("area_entered", self, "on_area_entered_hurtbox")
+	attack_delay_timer.connect("timeout", self, "attack")
+	attack_cooldown_timer.connect("timeout", self, "attack")
+
 # Called, usually externally, when the enemy needs to take damage
 func take_damage(attacker, damage: int):
 	state_machine.call_method("take_damage", [attacker, damage])
+
+# Called when the player's hitbox enters the enemy's hurtbox
+func on_area_entered_hurtbox(area):
+	attack_delay_timer.start()
+
+# Called when the attack timer finishes
+func attack():
+	state_machine.call_method("attack")
 
 # --- OLD ATTACKING CODE ---
 #func area_entered_hurtbox(area):
--- Actors/State Machines/Enemy States/Rock Hermit/RockHermitChaseState.gd
+++ Actors/State Machines/Enemy States/Rock Hermit/RockHermitChaseState.gd
@@ -7,6 +7,7 @@ const MAX_SPEED = 4000
 const MAX_STEERING = 5
 const AVOID_FORCE = MAX_SPEED
 const STOP_DISTANCE = 25
+const ATTACK_DAMAGE = 1
 
 func enter(ctx: Dictionary = {}):
 	if not ctx.has("target"): return state_machine.enter_state("Hidden")
@@ -15,6 +16,15 @@ func enter(ctx: Dictionary = {}):
 func update(delta):
 	update_sprite()
 	enemy.hurtbox.look_at(enemy.target.position)
+
+func attack():
+	var areas = enemy.hurtbox.get_overlapping_areas()
+	if areas.size() > 0 and enemy.attack_cooldown_timer.is_stopped():
+		var hitbox: Area2D = areas[0]
+		var attackee = hitbox.get_parent()
+		if attackee.has_method("take_damage"):
+			attackee.take_damage(enemy, ATTACK_DAMAGE)
+		enemy.attack_cooldown_timer.start()
 
 func update_sprite():
 	if enemy.target.position.x > enemy.position.x:
@@ -37,12 +47,11 @@ func physics_update(delta):
 
 func get_seek_steering(delta) -> Vector2:
 	var desired_velocity = Vector2.ZERO
-	if enemy.position.distance_to(enemy.target.position) > STOP_DISTANCE:
-		var direction = get_seek_direction()
-		if direction == Vector2.ZERO:
-			state_machine.enter_state("Hidden")
-		else:
-			desired_velocity = direction * MAX_SPEED * delta
+	var direction = get_seek_direction()
+	if direction == Vector2.ZERO: # Enemy is unable to locate the target
+		state_machine.enter_state("Hidden")
+	elif enemy.position.distance_to(enemy.target.position) > STOP_DISTANCE:
+		desired_velocity = direction * MAX_SPEED * delta
 	return desired_velocity - enemy.velocity
 
 func get_seek_direction() -> Vector2:
