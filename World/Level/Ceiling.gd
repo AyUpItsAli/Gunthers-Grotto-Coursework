08e1fda89c34c844692efd56526f69adad93df53
Ali <ali.dee1410@gmail.com>
2022-11-05 14:33:48 +0000

    Ceiling tiles placed on outside border
    Made it so ceiling tiles are placed on the outside border, otherwise you can see where there are missing ceiling tiles at the edge of the map.

--- World/Level/Ceiling.gd
+++ World/Level/Ceiling.gd
@@ -7,7 +7,7 @@ const CEILING = 0
 
 # Places ceiling tiles above wall tiles
 func update_ceiling_layer():
-	for x in range(Globals.CAVE_SIZE):
-		for y in range(Globals.CAVE_SIZE):
+	for x in range(-1, Globals.CAVE_SIZE+1):
+		for y in range(-1, Globals.CAVE_SIZE+1):
 			var tile = -1 if walls.get_cell(x, y) == -1 else CEILING
 			set_cell(x, y, tile)
