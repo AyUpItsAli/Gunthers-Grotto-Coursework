ebe3dc4fc2bba6f0cd31d4f9d34e072543b4cf18
Ali <ali.dee1410@gmail.com>
2022-11-16 17:41:19 +0000

    Select closest neighbour tile if attempting to mine diagonally
    - Previously, if the player tries selecting a tile that is blocked by two of its neighbours, for example when trying to mine diagonally, no tile was selected. Now, the closest of these two neighbours will be selected instead, to ensure at least one tile is selected.
    - Also, the solid() function has been updated to take in a Vector2 rather than two x and y values.

--- World/Level/MiningGrid.gd
+++ World/Level/MiningGrid.gd
@@ -34,13 +34,13 @@ func select_tile(tile_pos: Vector2):
 	selected_tile = tile_pos
 	tile_selected = true
 
-func is_solid_tile(x, y):
-	return ceiling.get_cell(x, y) != -1
+func is_solid_tile(tile_pos: Vector2):
+	return ceiling.get_cellv(tile_pos) != -1
 
 func get_player_tile_pos() -> Vector2:
 	var actual_tile_pos = world_to_map(player.position)
 	# Return the actual tile pos, if it's not solid
-	if not is_solid_tile(actual_tile_pos.x, actual_tile_pos.y):
+	if not is_solid_tile(actual_tile_pos):
 		player_tile = actual_tile_pos
 	else:
 		# If actual tile is solid, get the centre pos of the actual tile
@@ -51,14 +51,15 @@ func get_player_tile_pos() -> Vector2:
 		# Approximate direction and get the next tile in that direction 
 		var next_tile_pos = actual_tile_pos + Utils.approximate_direction_4_ways(direction)
 		# Return the next tile pos instead, if it's not solid
-		if not is_solid_tile(next_tile_pos.x, next_tile_pos.y):
+		if not is_solid_tile(next_tile_pos):
 			player_tile = next_tile_pos
 	return player_tile
 
 func update_selected_tile():
 	# Get the player's current tile pos and the tile hovered over by the mouse
 	var player_tile_pos = get_player_tile_pos()
-	var mouse_tile_pos = world_to_map(get_global_mouse_position())
+	var mouse_pos = get_global_mouse_position()
+	var mouse_tile_pos = world_to_map(mouse_pos)
 	# Get the direction from the current tile to the hovered over tile
 	var exact_direction = player_tile_pos.direction_to(mouse_tile_pos)
 	var direction = Utils.approximate_direction_8_ways(exact_direction)
@@ -70,21 +71,31 @@ func update_selected_tile():
 	var out_of_bounds = (1 > tile_pos.x or tile_pos.x >= Globals.CAVE_SIZE-1
 						or 1 > tile_pos.y or tile_pos.y >= Globals.CAVE_SIZE-1)
 	# Check if the tile is solid (otherwise there is nothing to mine)
-	var solid = is_solid_tile(tile_pos.x, tile_pos.y)
-	# Check if the tile is blocked from the player by its neighbouring tiles
-	var blocked = (is_solid_tile(tile_pos.x - direction.x, tile_pos.y)
-					and is_solid_tile(tile_pos.x, tile_pos.y - direction.y))
+	var solid = is_solid_tile(tile_pos)
 	# Get the closest edge or vertex of the tile,
 	# and store the distance from the player to this point
 	var offset = (Vector2.ONE - direction.normalized()) * cell_size/2
 	var closest_edge_or_vertex = map_to_world(tile_pos) + offset
 	var distance = player.position.distance_to(closest_edge_or_vertex)
 	
-	# If the tile is not out of bounds, solid, not blocked from the player,
-	# and the distance from the player to the closest edge or vertex is less
-	# than the maximum distance, the tile can be selected...
-	if not out_of_bounds and solid and not blocked and distance <= MAX_DISTANCE:
-		select_tile(tile_pos)
+	# If the tile is not out of bounds, is solid, and the distance from
+	# the player to the closest edge or vertex is less than the maximum
+	# distance, the tile can be selected from...
+	if not out_of_bounds and solid and distance <= MAX_DISTANCE:
+		var nbr_tile_1 = Vector2(tile_pos.x - direction.x, tile_pos.y)
+		var nbr_tile_2 = Vector2(tile_pos.x, tile_pos.y - direction.y)
+		# If the tile is blocked from the player by its neighbouring tiles...
+		if is_solid_tile(nbr_tile_1) and is_solid_tile(nbr_tile_2):
+			offset = Vector2.ONE * cell_size / 2
+			var nbr_centre_1 = map_to_world(nbr_tile_1) + offset
+			var nbr_centre_2 = map_to_world(nbr_tile_2) + offset
+			# ...select the neighbour tile closest to the mouse position.
+			if mouse_pos.distance_to(nbr_centre_1) < mouse_pos.distance_to(nbr_centre_2):
+				select_tile(nbr_tile_1)
+			else:
+				select_tile(nbr_tile_2)
+		else: # ... otherwise, select the original tile
+			select_tile(tile_pos)
 	else: # ... otherwise, ensure no tiles are selected
 		deselect_tile()
 
